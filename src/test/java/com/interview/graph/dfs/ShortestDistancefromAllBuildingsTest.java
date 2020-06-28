2fdeb0730d8a53e2e0ec56271b18617d09adb517
meghnadsaha <meghnadsaha422@gmail.com>
2020-06-28 18:34:47 +0530

    Shortest Distance from All Buildings

---
+++ src/test/java/com/interview/graph/dfs/ShortestDistancefromAllBuildingsTest.java
@@ -0,0 +1,24 @@
+package com.interview.graph.dfs;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.interview.graph.ShortestDistancefromAllBuildings;
+
+public class ShortestDistancefromAllBuildingsTest {
+
+	@Test
+	public void test() {
+		ShortestDistancefromAllBuildings all = new ShortestDistancefromAllBuildings();
+		int[][] prerequisites = { { 1, 0, 2, 0, 1 }, { 0, 0, 0, 0, 0 }, { 0, 0, 1, 0, 0 }, };
+		int[][] prerequisites2 = { { 1, 0, 2, 0, 1 }, { 2, 0, 0, 2, 0 }, { 0, 2, 1, 0, 2 } };
+		Assert.assertEquals(7, all.shortestDistance(prerequisites));
+		Assert.assertEquals(-1, all.shortestDistance(prerequisites2));
+
+	}
+
+}
