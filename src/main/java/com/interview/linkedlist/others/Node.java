dba6212c2c1f335bd931789df1349e88def7be6e
Meghnad.Saha <meghnadsaha422@gmail.com>
2021-12-11 19:23:35 +0530

    146. LRU Cache https://leetcode.com/problems/lru-cache/

--- src/main/java/com/interview/linkedlist/others/Node.java
+++ src/main/java/com/interview/linkedlist/others/Node.java
@@ -1,10 +1,28 @@
 package com.interview.linkedlist.others;
 
 public class Node {
-    public int val;
-    public Node next;
-    public Node() {}
-    Node(int val) { this.val = val; }
 
-	Node(int val, Node next) { this.val = val; this.next = next; }
+    public int key;
+    public int val;
+
+    public Node prev;
+    public Node next;
+    public Node random;
+
+    public Node() {
+    }
+
+    public Node(int val) {
+        this.val = val;
+    }
+
+    public Node(int val, Node next) {//For Normal Nodes
+        this.val = val;
+        this.next = next;
+    }
+
+    public Node(int key, int val) { // For Hash-Map
+        this.key = key;
+        this.val = val;
+    }
 }	
