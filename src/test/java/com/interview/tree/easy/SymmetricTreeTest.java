f5165ce7bff2e018a349e3adda01d1b86ee682f2
Meghnad.Saha <meghnadsaha422@gmail.com>
2021-12-12 21:06:25 +0530

    101. Symmetric Tree https://leetcode.com/problems/symmetric-tree/

---
+++ src/test/java/com/interview/tree/easy/SymmetricTreeTest.java
@@ -0,0 +1,40 @@
+package com.interview.tree.easy;
+
+import com.interview.tree.common.TreeNode;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class SymmetricTreeTest {
+
+    @Test
+    public void testIsSymmetric() {
+        SymmetricTree symmetricTree = new SymmetricTree();
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(2);
+        root.left.left = new TreeNode(3);
+        root.left.right = new TreeNode(4);
+        root.right.left = new TreeNode(4);
+        root.right.right = new TreeNode(3);
+
+        boolean actualOutput = symmetricTree.isSymmetric(root);
+
+        Assert.assertEquals(true,actualOutput);
+    }
+
+    @Test
+    public void testMaxDepthOfBinaryTree() {
+        MaxDepthOfBinaryTree maxDepthOfBinaryTree = new MaxDepthOfBinaryTree();
+        TreeNode root = new TreeNode(3);
+        root.left = new TreeNode(9);
+        root.right = new TreeNode(20);
+        root.right.left = new TreeNode(15);
+        root.right.right = new TreeNode(7);
+
+        int actualOutput = maxDepthOfBinaryTree.maxDepth(root);
+
+        Assert.assertEquals(3,actualOutput);
+    }
+
+
+}
