0cab769a39cc0c6533db79f95d283e2d4f32bc51
Meghnad.Saha <meghnadsaha422@gmail.com>
2022-01-05 20:50:22 +0530

    378. Kth Smallest Element in a Sorted Matrix
    https://leetcode.com/problems/kth-smallest-element-in-a-sorted-matrix/

---
+++ src/test/java/com/interview/heap/KthSmallestElementInASortedMatrixTest.java
@@ -0,0 +1,26 @@
+package com.interview.heap;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class KthSmallestElementInASortedMatrixTest {
+    @Test
+    public void testDifferentCases() {
+
+        KthSmallestElementInASortedMatrix kthSmallestElementInASortedMatrix = new KthSmallestElementInASortedMatrix();
+        int[][] matrix = {{1, 5, 9}, {10, 11, 13}, {12, 13, 15}};
+        int k = 8;
+        int outPut = kthSmallestElementInASortedMatrix.kthSmallest(matrix, k);
+        Assert.assertEquals(13, outPut);
+    }
+    @Test
+    public void testDifferentCases2() {
+
+        KthSmallestElementInASortedMatrix kthSmallestElementInASortedMatrix = new KthSmallestElementInASortedMatrix();
+        int[][] matrix = {{-5}};
+        int k = 1;
+        int outPut = kthSmallestElementInASortedMatrix.kthSmallest(matrix, k);
+        Assert.assertEquals(-5, outPut);
+    }
+
+}
