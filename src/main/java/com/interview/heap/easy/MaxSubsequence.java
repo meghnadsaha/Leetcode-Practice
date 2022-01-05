8203ffe612f97123dfb6f36c4058c5cdd0aba848
Meghnad.Saha <meghnadsaha422@gmail.com>
2022-01-05 21:42:11 +0530

    https://leetcode.com/problems/find-subsequence-of-length-k-with-the-largest-sum/submissions/

---
+++ src/main/java/com/interview/heap/easy/MaxSubsequence.java
@@ -0,0 +1,31 @@
+package com.interview.heap.easy;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.PriorityQueue;
+
+public class MaxSubsequence {
+    public int[] maxSubsequence(int[] nums, int k) {
+        int n = nums.length;
+        // [index, number]
+        PriorityQueue<int[]> heap = new PriorityQueue<>(Comparator.comparingInt(a -> a[1]));
+        for (int i = 0; i < n; i++) {
+            heap.add(new int[]{i, nums[i]});
+            if (heap.size() > k) {
+                heap.poll();
+            }
+        }
+
+        // sort by index
+        List<int[]> list = new ArrayList<>(heap);
+        list.sort(Comparator.comparingInt(a -> a[0]));
+
+        int[] result = new int[k];
+        for (int i = 0; i < k; i++) {
+            result[i] = list.get(i)[1];
+        }
+
+        return result;
+    }
+}
