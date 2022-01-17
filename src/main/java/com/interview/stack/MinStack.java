5c1dd0661077ddcfcd59a46dc5c570c859f0ed8f
Meghnad.Saha <meghnadsaha422@gmail.com>
2022-01-17 19:56:28 +0530

    https://leetcode.com/problems/min-stack/submissions/

---
+++ src/main/java/com/interview/stack/MinStack.java
@@ -0,0 +1,46 @@
+package com.interview.stack;
+
+public class MinStack {
+
+    class Node {
+        int val;
+        int min;
+        Node next;
+
+        private Node(int val, int min, Node next) {
+            this.val = val;
+            this.min = min;
+            this.next = next;
+        }
+    }
+
+    Node head;
+
+    public void push(int val) {
+        if (head == null)
+            head = new Node(val, val, null);
+        else
+            head = new Node(val, Math.min(val, head.min), head);
+    }
+
+    public void pop() {
+        head = head.next;
+    }
+
+    public int top() {
+        return head.val;
+    }
+
+    public int getMin() {
+        return head.min;
+    }
+}
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(val);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
