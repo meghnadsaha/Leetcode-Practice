095ec77a2a35a8ffde81f1cc53f79ac863395b8c
Meghnad.Saha <meghnadsaha422@gmail.com>
2022-01-30 09:34:24 +0530

    (Alternate Method added)Letter Combinations of a Phone Number https://leetcode.com/problems/letter-combinations-of-a-phone-number/

--- src/main/java/com/interview/string/LetterCombinationsofaPhoneNumber.java
+++ src/main/java/com/interview/string/LetterCombinationsofaPhoneNumber.java
@@ -3,8 +3,8 @@ package com.interview.string;
 import java.util.*;
 
 public class LetterCombinationsofaPhoneNumber {
-	public List<String> letterCombinations(String digits) {
-		HashMap<Integer, Character[]> map = new HashMap<Integer, Character[]>();
+	public static List<String> letterCombinations2(String digits) {
+		HashMap<Integer, Character[]> map = new HashMap<>();
 		map.put(2, new Character[] { 'a', 'b', 'c' });
 		map.put(3, new Character[] { 'd', 'e', 'f' });
 		map.put(4, new Character[] { 'g', 'h', 'i' });
@@ -14,25 +14,47 @@ public class LetterCombinationsofaPhoneNumber {
 		map.put(8, new Character[] { 't', 'u', 'v' });
 		map.put(9, new Character[] { 'w', 'x', 'y', 'z' });
 
-		List<String> returnList = new LinkedList<String>();
+		List<String> returnList = new LinkedList<>();
 		combHelper(0, map, returnList, digits, "");
 		return returnList;
 
 	}
 
-	public void combHelper(int start, Map map, List returnList, String string, String temp) {
+	public static void combHelper(int start, Map<Integer, Character[]> map, List<String> returnList, String string, String temp) {
 
 		if (start > string.length() - 1)
 			return;
 
 		int curr = Character.getNumericValue(string.charAt(start));
 
-		for (Character element : (Character[]) map.get(curr)) {
+		for (Character element :  map.get(curr)) {
 			combHelper(start + 1, map, returnList, string, temp + element);
 			if (start == string.length() - 1)
 				returnList.add(temp + element);
 		}
 
 	}
+
+
+	public static List<String> letterCombinations(String digits) {
+		LinkedList<String> ans = new LinkedList<>();
+		if(digits.isEmpty()) return ans;
+		String[] mapping = new String[] {"0", "1", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+		ans.add("");
+		for(int i =0; i<digits.length();i++){
+			int x = Character.getNumericValue(digits.charAt(i));
+			while(ans.peek().length()==i){
+				String t = ans.remove();
+				for(char s : mapping[x].toCharArray())
+					ans.add(t+s);
+			}
+		}
+		return ans;
+	}
+
+	public static void main(String[] args){
+		System.out.println(letterCombinations2("29"));
+	}
+
 
 }
