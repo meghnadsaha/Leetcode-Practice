d5ac9418546f3193a98926e5f1794acf8c75d43c
Meghnad.Saha <meghnadsaha422@gmail.com>
2022-02-01 09:42:19 +0530

    131. Palindrome Partitioning https://leetcode.com/problems/palindrome-partitioning/

---
+++ src/main/java/com/interview/string/PalindromePartitioning.java
@@ -0,0 +1,52 @@
+package com.interview.string;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PalindromePartitioning {
+    public static List<List<String>> partition(String s) {
+        int len = s.length();
+        boolean[][] dp = new boolean[len][len];
+        List<List<String>> result = new ArrayList<>();
+//        dfsUsingBackTracking(0, result, new ArrayList<>(), s);
+        dfsUsingDynamicProgramming(result, s, 0, new ArrayList<>(), dp);
+        return result;
+    }
+
+    static void dfsUsingDynamicProgramming(List<List<String>> result, String s, int start, List<String> currentList, boolean[][] dp) {
+        if (start >= s.length()) result.add(new ArrayList<>(currentList));
+        for (int end = start; end < s.length(); end++) {
+            if (s.charAt(start) == s.charAt(end) && (end - start <= 2 || dp[start + 1][end - 1])) {
+                dp[start][end] = true;
+                currentList.add(s.substring(start, end + 1));
+                dfsUsingDynamicProgramming(result, s, end + 1, currentList, dp);
+                currentList.remove(currentList.size() - 1);
+            }
+        }
+    }
+
+    static void dfsUsingBackTracking(int start, List<List<String>> result, List<String> currentList, String s) {
+        if (start >= s.length()) result.add(new ArrayList<>(currentList));
+        for (int end = start; end < s.length(); end++) {
+            if (isPalindrome(s, start, end)) {
+                // add current substring in the currentList
+                currentList.add(s.substring(start, end + 1));
+                dfsUsingBackTracking(end + 1, result, currentList, s);
+                // backtrack and remove the current substring from currentList
+                currentList.remove(currentList.size() - 1);
+            }
+        }
+    }
+
+    static boolean isPalindrome(String s, int low, int high) {
+        while (low < high) {
+            if (s.charAt(low++) != s.charAt(high--)) return false;
+        }
+        return true;
+    }
+
+
+    public static void main(String args[]){
+         System.out.println(partition("aab"));
+    }
+}
