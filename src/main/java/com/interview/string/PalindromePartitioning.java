7218b5619cfb237a991f66b9eac6ca52eca6f063
Meghnad.Saha <meghnadsaha422@gmail.com>
2022-02-01 09:42:19 +0530

    131. Palindrome Partitioning https://leetcode.com/problems/palindrome-partitioning/

---
+++ src/main/java/com/interview/string/PalindromePartitioning.java
@@ -0,0 +1,37 @@
+package com.interview.string;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PalindromePartitioning {
+    public static List<List<String>> partition(String s) {
+        List<List<String>> result = new ArrayList<>();
+        dfs(0, result, new ArrayList<>(), s);
+        return result;
+    }
+
+    static void dfs(int start, List<List<String>> result, List<String> currentList, String s) {
+        if (start >= s.length()) result.add(new ArrayList<>(currentList));
+        for (int end = start; end < s.length(); end++) {
+            if (isPalindrome(s, start, end)) {
+                // add current substring in the currentList
+                currentList.add(s.substring(start, end + 1));
+                dfs(end + 1, result, currentList, s);
+                // backtrack and remove the current substring from currentList
+                currentList.remove(currentList.size() - 1);
+            }
+        }
+    }
+
+    static boolean isPalindrome(String s, int low, int high) {
+        while (low < high) {
+            if (s.charAt(low++) != s.charAt(high--)) return false;
+        }
+        return true;
+    }
+
+
+    public static void main(String args[]){
+         System.out.println(partition("aab"));
+    }
+}
