09008bccf55a1bdc58d3af7ba4555d48bd6ef7bb
Meghnad.Saha <meghnadsaha422@gmail.com>
2022-02-10 09:47:10 +0530

    300. Longest Increasing Subsequence

---
+++ src/main/java/com/interview/dynamic/programming/medium/LongestIncreasingSubsequence.java
@@ -0,0 +1,21 @@
+package com.interview.dynamic.programming.medium;
+
+import java.util.Arrays;
+
+public class LongestIncreasingSubsequence {
+    public int lengthOfLIS(int[] nums) {
+        int[] dp = new int[nums.length];
+        int len = 0;
+
+        for (int x : nums) {
+            int i = Arrays.binarySearch(dp, 0, len, x);
+            if (i < 0)
+                i = -(i + 1);
+            dp[i] = x;
+            if (i == len)
+                len++;
+        }
+
+        return len;
+    }
+}
