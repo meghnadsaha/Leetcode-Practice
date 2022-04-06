11e3dedfbbc53ec53dbb46978b6f16a419af7953
Meghnad.Saha <meghnadsaha422@gmail.com>
2022-04-06 20:51:47 +0530

    95. Unique Binary Search Trees II https://leetcode.com/problems/unique-binary-search-trees-ii/

---
+++ src/main/java/com/interview/tree/medium/UniqueBinarySearchTreesII.java
@@ -0,0 +1,44 @@
+package com.interview.tree.medium;
+
+import com.interview.tree.common.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UniqueBinarySearchTreesII {
+
+    public static List<TreeNode> generateTrees(int n) {
+        List<TreeNode>[] result = new List[n + 1];
+        result[0] = new ArrayList<>();
+        if (n == 0) {
+            return result[0];
+        }
+
+        result[0].add(null);
+        for (int len = 1; len <= n; len++) {
+            result[len] = new ArrayList<>();
+            for (int j = 0; j < len; j++) {
+                for (TreeNode nodeL : result[j]) {
+                    for (TreeNode nodeR : result[len - j - 1]) {
+                        TreeNode node = new TreeNode(j + 1);
+                        node.left = nodeL;
+                        node.right = clone(nodeR, j + 1);
+                        result[len].add(node);
+                    }
+                }
+            }
+        }
+        return result[n];
+    }
+
+    private static TreeNode clone(TreeNode n, int offset) {
+        if (n == null) {
+            return null;
+        }
+        TreeNode node = new TreeNode(n.val + offset);
+        node.left = clone(n.left, offset);
+        node.right = clone(n.right, offset);
+        return node;
+    }
+
+}
