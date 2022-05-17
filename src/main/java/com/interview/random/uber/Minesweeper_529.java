b6b786494e29515d3e137ec1b9df36dd8c203200
vn51zde <meghnad>
2022-05-17 22:54:49 +0530

    529. Minesweeper https://leetcode.com/problems/minesweeper/

---
+++ src/main/java/com/interview/random/uber/Minesweeper_529.java
@@ -0,0 +1,70 @@
+package com.interview.random.uber;
+
+public class Minesweeper_529 {
+
+    /**
+     * If click on a mine ('M'), mark it as 'X', stop further search.
+     * If click on an empty cell ('E'), depends on how many surrounding mine:
+     * 2.1 Has surrounding mine(s), mark it with number of surrounding mine(s), stop further search.
+     * 2.2 No surrounding mine, mark it as 'B', continue search its 8 neighbors.
+     */
+
+    public static char[][] updateBoard(char[][] board, int[] click) {
+        int m = board.length, n = board[0].length;
+        int row = click[0], col = click[1];
+
+
+        if (board[row][col] == 'M') {
+            board[row][col] = 'X';
+        } else {
+            int count = 0;
+            for (int i = -1; i < 2; i++) {
+                for (int j = -1; j < 2; j++) {
+                    if (i == 0 && j == 0) continue;
+                    int r = row + i, c = col + j;
+                    if (r < 0 || r >= m || c < 0 || c >= n) continue;
+                    if (board[r][c] == 'M' || board[r][c] == 'X') count++;
+                }
+            }
+
+
+            if (count > 0) {
+                board[row][col] = (char) (count + '0');
+            } else {
+                board[row][col] = 'B';
+                for (int i = -1; i < 2; i++) {
+                    for (int j = -1; j < 2; j++) {
+                        if (i == 0 && j == 0) continue;
+                        int r = row + i, c = col + j;
+                        if (r < 0 || r >= m || c < 0 || c >= n) continue;
+                        if (board[r][c] == 'E') updateBoard(board, new int[]{r, c});
+                    }
+                }
+            }
+        }
+        return board;
+
+    }
+
+    public static void main(String[] args) {
+        char[][] board = {
+                {'E', 'E', 'E', 'E', 'E'},
+                {'E', 'E', 'M', 'E', 'E'},
+                {'E', 'E', 'E', 'E', 'E'},
+                {'E', 'E', 'E', 'E', 'E'}};
+        int[] click = {3, 0};
+
+        char[][] output = updateBoard(board, click);
+
+
+        char[][] board2 = {
+                {'B', '1', 'E', '1', 'B'},
+                {'B', '1', 'M', '1', 'B'},
+                {'B', '1', '1', '1', 'B'},
+                {'B', 'B', 'B', 'B', 'B'}
+        };
+        int[] click2 = {1, 2};
+
+        char[][] output2 =  updateBoard(board2, click2);
+    }
+}
---
+++ src/main/java/com/interview/random/uber/SlidingPuzzle.java
@@ -0,0 +1,89 @@
+package com.interview.random.uber;
+
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Set;
+
+//int[][] dirs = new int[][] { { 1, 3 }, { 0, 2, 4 },{ 1, 5 }, { 0, 4 }, { 1, 3, 5 }, { 2, 4 } };
+
+//        0 1 2
+//        3 4 5 --> 0 can go to {1, 3}
+//
+//        1 0 2
+//        3 4 5 --> 0 can go to index of {0,2,4}
+//
+//        1 2 0
+//        3 4 5 --> 0 can go to {1, 5}
+//
+//        1 2 3
+//        0 4 5 --> 0 can go to {0,4}
+//
+//        1 2 3
+//        4 0 5 --> 0 can go to {1, 3, 5}
+//
+//        1 2 3
+//        4 5 0 --> 0 can go to {2, 4}
+public class SlidingPuzzle {
+    public static int slidingPuzzle(int[][] board) {
+        String target = "123450";
+        StringBuilder start = new StringBuilder();
+
+        for (int[] ints : board) {
+            for (int j = 0; j < board[0].length; j++) {
+                start.append(ints[j]);
+            }
+        }
+
+        Set<String> visited = new HashSet<>();
+        int[][] dirs = new int[][]{
+                {1, 3}, {0, 2, 4}, {1, 5}, {0, 4}, {1, 3, 5}, {2, 4}
+        };
+        Queue<String> queue = new LinkedList<>();
+        queue.offer(start.toString());
+        visited.add(start.toString());
+        int res = 0;
+
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                String cur = queue.poll();
+                assert cur != null;
+                if (cur.equals(target)) {
+                    return res;
+                }
+                int zero = cur.indexOf('0');
+
+                for (int dir : dirs[zero]) {
+                    String next = swap(cur, zero, dir);
+                    if (visited.contains(next)) {
+                        continue;
+                    }
+                    visited.add(next);
+                    queue.offer(next);
+                }
+            }
+            res++;
+        }
+        return -1;
+    }
+
+    private static String swap(String str, int zero, int dir) {
+        StringBuilder sb = new StringBuilder(str);
+        sb.setCharAt(zero, str.charAt(dir));
+        sb.setCharAt(dir, str.charAt(zero));
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        int[][] board = {
+                {1, 2, 3},
+                {4, 0, 5}};
+
+
+        int output = slidingPuzzle(board);
+        System.out.println(output);
+
+    }
+}
+
