cfaf93d7de189b37c71918b41a673fc1f9cbb6f2
vn51zde <meghnad>
2022-05-22 23:57:31 +0530

    121. Best Time to Buy and Sell Stock https://leetcode.com/problems/best-time-to-buy-and-sell-stock/

---
+++ src/main/java/com/interview/random/google/BestTimeToBuyAndSellStock121.java
@@ -0,0 +1,15 @@
+package com.interview.random.google;
+
+public class BestTimeToBuyAndSellStock121 {
+    public int maxProfit(int[] prices) {
+        int minprice = Integer.MAX_VALUE;
+        int maxprofit = 0;
+        for (int i = 0; i < prices.length; i++) {
+            if (prices[i] < minprice)
+                minprice = prices[i];
+            else if (prices[i] - minprice > maxprofit)
+                maxprofit = prices[i] - minprice;
+        }
+        return maxprofit;
+    }
+}
