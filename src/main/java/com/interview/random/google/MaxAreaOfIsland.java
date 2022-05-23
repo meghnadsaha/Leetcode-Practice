86daf21128bc68f5cb5bfb4c88ef33199b9fa76a
vn51zde <meghnad>
2022-05-23 07:47:53 +0530

    695. Max Area of Island https://leetcode.com/problems/max-area-of-island/

---
+++ src/main/java/com/interview/random/google/MaxAreaOfIsland.java
@@ -0,0 +1,47 @@
+package com.interview.random.google;
+
+public class MaxAreaOfIsland {
+    public static int maxAreaOfIsland(int[][] grid) {
+        int maxArea = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == 1) {
+                    maxArea = Math.max(maxArea, dfsForAreaOfIsland(grid, i, j));
+                }
+            }
+        }
+        return maxArea;
+    }
+
+    public static int dfsForAreaOfIsland(int[][] grid, int i, int j) {
+
+        if (i >= 0 && i < grid.length && j >= 0 && j < grid[0].length && grid[i][j] == 1) {
+            grid[i][j] = 0;
+            return 1 + dfsForAreaOfIsland(grid, i - 1, j)
+                    + dfsForAreaOfIsland(grid, i + 1, j)
+                    + dfsForAreaOfIsland(grid, i, j - 1)
+                    + dfsForAreaOfIsland(grid, i, j + 1);
+        }
+        return 0;
+    }
+
+    public static void main(String args[]) {
+        int[][] grid1 = {
+                {0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0}};
+        int[][] grid = {
+                {0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0},
+                {0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 0, 0},
+                {0, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0}
+        };
+
+        System.out.println(maxAreaOfIsland(grid1));
+
+    }
+
+}
