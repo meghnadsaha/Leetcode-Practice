a8cb7f388732c002c8abaad92a662f3386ed37db
vn51zde <meghnad>
2022-06-01 08:51:12 +0530

    986. Interval List Intersections https://leetcode.com/problems/interval-list-intersections/

--- src/main/java/com/interview/random/uber/IntervalListIntersections_986.java
+++ src/main/java/com/interview/random/uber/IntervalListIntersections_986.java
@@ -1,5 +1,30 @@
 package com.interview.random.uber;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class IntervalListIntersections_986 {
 
+    public int[][] intervalIntersection(int[][] firstList, int[][] secondList) {
+        List<int[]> ans = new ArrayList();
+        int i = 0, j = 0;
+
+        while (i < firstList.length && j < secondList.length) {
+            // Let's check if A[i] intersects B[j].
+            // lo - the startpoint of the intersection
+            // hi - the endpoint of the intersection
+            int lo = Math.max(firstList[i][0], secondList[j][0]);
+            int hi = Math.min(firstList[i][1], secondList[j][1]);
+            if (lo <= hi)
+                ans.add(new int[]{lo, hi});
+
+            // Remove the interval with the smallest endpoint
+            if (firstList[i][1] < secondList[j][1])
+                i++;
+            else
+                j++;
+        }
+
+        return ans.toArray(new int[ans.size()][]);
+    }
 }
