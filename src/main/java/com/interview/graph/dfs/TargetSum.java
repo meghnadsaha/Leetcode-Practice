17f803e873f0051445d313c75c1713a7e7255ef0
vn51zde <meghnad>
2022-06-16 20:59:36 +0530

    Target Sum has  been modified

--- src/main/java/com/interview/graph/dfs/TargetSum.java
+++ src/main/java/com/interview/graph/dfs/TargetSum.java
@@ -3,6 +3,23 @@ package com.interview.graph.dfs;
 import java.util.*;
 
 public class TargetSum {
+
+
+	public int findTargetSumWays2(int[] nums, int target) {
+		Map<Integer, Integer> dp = new HashMap();
+		dp.put(0, 1);
+		for (int num : nums) {
+			Map<Integer, Integer> dp2 = new HashMap();
+			for (int tempSum : dp.keySet()) {
+				int key1 = tempSum + num;
+				dp2.put(key1, dp2.getOrDefault(key1, 0) + dp.get(tempSum));
+				int key2 = tempSum - num;
+				dp2.put(key2, dp2.getOrDefault(key2, 0) + dp.get(tempSum));
+			}
+			dp = dp2;
+		}
+		return dp.getOrDefault(target, 0);
+	}
 	public int findTargetSumWays(int[] nums, int S) {
 		if (nums.length == 0)
 			return 0;
