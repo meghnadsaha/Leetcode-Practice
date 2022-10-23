f1e82df125a70f397da0f84da7079a622f461260
msahaopsecsecurity <msaha@opsecsecurity.com>
2022-10-23 20:14:14 +0530

    1004. Max Consecutive Ones III

---
+++ src/main/java/com/interview/sliding/window/MaxConsecutiveOnesIII.java
@@ -0,0 +1,34 @@
+package com.interview.sliding.window;
+
+public class MaxConsecutiveOnesIII {
+
+    public static int longestOnes(int[] nums, int k) {
+        if (nums == null) {
+            throw new IllegalArgumentException("Input array is null");
+        }
+        int start = 0;
+        int end = 0;
+        int zeros = 0;
+
+        while (end < nums.length) {
+            if (nums[end] == 0) {
+                zeros++;
+            }
+            end++;
+            if (zeros > k) {
+                if (nums[start] == 0) {
+                    zeros--;
+                }
+                start++;
+            }
+        }
+        return end - start;
+    }
+
+
+    public static void main(String args[]) {
+        int[] nums = new int[]{0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 1, 1, 0, 0, 0, 1, 1, 1, 1};
+        int k = 3;
+        System.out.println(longestOnes(nums, k));
+    }
+}
