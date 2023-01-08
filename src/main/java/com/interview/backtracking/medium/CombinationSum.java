75b7aa66b5c17a5ad34e90cadc197bdbd79cf444
meghnadsaha <meghnadsaha422@gmail.com>
2023-01-08 10:49:42 +0530

    39. Combination Sum

--- src/main/java/com/interview/backtracking/medium/CombinationSum.java
+++ src/main/java/com/interview/backtracking/medium/CombinationSum.java
@@ -23,7 +23,20 @@ public class CombinationSum {
         }
         // DFS all the paths
         for (int i = index; i < candidates.length; i++) {
-            int curSum = target - candidates[i];
+         //   for (int i = 0; i < candidates.length; i++) {
+            /**
+             *
+             * If we Start from indices i = 0 then out put will be like this
+             * candidates =
+             * [2,3,6,7]
+             * target =
+             * 7
+             * Output
+             * [[2,2,3],[2,3,2],[3,2,2],[7]]
+             */
+
+
+                int curSum = target - candidates[i];
             // Check if out of bound
             if (curSum < 0) continue;
             comb.add(candidates[i]);
