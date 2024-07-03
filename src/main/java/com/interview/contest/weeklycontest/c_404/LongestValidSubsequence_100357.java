e8eb1a248c8e033cf9e58ee070ac3b29ff42dfb5
meghnadsaha <meghnadsaha422@gmail.com>
2024-07-03 23:01:38 +0530

    LongestValidSubsequence_100357

--- src/main/java/com/interview/contest/weeklycontest/c_404/LongestValidSubsequence_100357.java
+++ src/main/java/com/interview/contest/weeklycontest/c_404/LongestValidSubsequence_100357.java
@@ -1,38 +1,60 @@
 package com.interview.contest.weeklycontest.c_404;
 
+import org.junit.Test;
+
+import java.util.Arrays;
+
+import static junit.framework.TestCase.assertEquals;
+
 public class LongestValidSubsequence_100357 {
 
-//https://leetcode.com/contest/weekly-contest-404/problems/find-the-maximum-length-of-valid-subsequence-i/description/
-    public static int maximumLength(int[] nums) {
-        if (nums == null || nums.length < 2) {
-            return nums.length; // Return length of the array if there are fewer than 2 elements
+    //https://leetcode.com/contest/weekly-contest-404/problems/find-the-maximum-length-of-valid-subsequence-i/description/
+    public int maximumLength ( int[] nums ) {
+        int n = nums.length;
+        int oddIdx = n, evenIdx = n, maxLength = 0;
+        int[][] dp = new int[n][2]; // dp[i][0] for sum % 2 == 0, dp[i][1] for sum % 2 == 1
+
+        // Initialize dp array
+        for (int i = 0 ; i < n ; i++) {
+            Arrays.fill(dp[i] , 1);
         }
 
-        int maxLength = 1;
-        int currentLength = 1;
-
-        for (int i = 1; i < nums.length; i++) {
-            if ((nums[i] + nums[i - 1]) % 2 == 1) {
-                currentLength++;
-            } else {
-                maxLength = Math.max(maxLength, currentLength);
-                currentLength = 1;
+        for (int i = n - 1 ; i >= 0 ; i--) {
+            if (nums[i] % 2 == 1) { // odd number
+                if (evenIdx != n) {
+                    dp[i][1] += dp[evenIdx][1];
+                }
+                if (oddIdx != n) {
+                    dp[i][0] += dp[oddIdx][0];
+                }
+                oddIdx = i;
+            } else { // even number
+                if (oddIdx != n) {
+                    dp[i][1] += dp[oddIdx][1];
+                }
+                if (evenIdx != n) {
+                    dp[i][0] += dp[evenIdx][0];
+                }
+                evenIdx = i;
             }
+            maxLength = Math.max(maxLength , Math.max(dp[i][0] , dp[i][1]));
         }
-
-        maxLength = Math.max(maxLength, currentLength);
 
         return maxLength;
     }
 
-    public static void main(String[] args) {
-        int[] nums1 = {1, 2, 3, 4};
-        System.out.println(maximumLength(nums1));  // Output: 4
+    @Test
+    public void testMaximumLength1 () {
 
-        int[] nums2 = {1, 2, 1, 1, 2, 1, 2};
-        System.out.println(maximumLength(nums2));  // Output: 6
+        LongestValidSubsequence_100357 longestValidSubsequence = new LongestValidSubsequence_100357();
 
-        int[] nums3 = {1, 3};
-        System.out.println(maximumLength(nums3));  // Output: 2
+        int[] nums = {1 , 2 , 3 , 4};
+        assertEquals(4 , longestValidSubsequence.maximumLength(nums));
+
+        int[] nums1 = {1 , 2 , 1 , 1 , 2 , 1 , 2};
+        assertEquals(61 , longestValidSubsequence.maximumLength(nums1));
+
+        int[] nums2 = {1 , 3};
+        assertEquals(2 , longestValidSubsequence.maximumLength(nums2));
     }
 }
